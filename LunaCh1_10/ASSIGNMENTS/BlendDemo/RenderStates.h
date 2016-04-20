1fd08e008e80b3e37c5207f38bbd69f2885b5d1b
Dylan Main PC <d.b.gijsbertsen@live.nl>
2016-04-20 15:53:02 +0200

    updated blenddemo

--- LunaCh1_10/ASSIGNMENTS/BlendDemo/RenderStates.h
+++ LunaCh1_10/ASSIGNMENTS/BlendDemo/RenderStates.h
@@ -4,7 +4,7 @@
 // Defines render state objects.  
 //***************************************************************************************
 
-#ifndef RENDERSTATES_H
+/*#ifndef RENDERSTATES_H
 #define RENDERSTATES_H
 
 #include "d3dUtil.h"
@@ -36,4 +36,72 @@ public:
 	static ID3D11DepthStencilState* NoDoubleBlendDSS;
 };
 
+#endif // RENDERSTATES_H*/
+
+
+//***************************************************************************************
+// RenderStates.h by Frank Luna (C) 2011 All Rights Reserved.
+//   
+// Defines render state objects.  
+//***************************************************************************************
+
+#ifndef RENDERSTATES_H
+#define RENDERSTATES_H
+
+#include "d3dUtil.h"
+
+class RenderStates
+{
+public:
+	static void InitAll(ID3D11Device* device);
+	static void DestroyAll();
+
+	// Rasterizer states
+	static ID3D11RasterizerState* WireframeRS;
+	static ID3D11RasterizerState* NoCullRS;
+	static ID3D11RasterizerState* CullClockwiseRS;
+
+	// Blend states
+	static ID3D11BlendState* AlphaToCoverageBS;
+	static ID3D11BlendState* TransparentBS;
+	static ID3D11BlendState* NoRenderTargetWritesBS;
+
+	// Eigen state
+	static ID3D11BlendState* TransparentBSMulti;
+	static ID3D11BlendState* TransparentBSAdditive;
+
+	// Depth/stencil states
+	static ID3D11DepthStencilState* MarkMirrorDSS;
+	static ID3D11DepthStencilState* DrawReflectionDSS;
+	static ID3D11DepthStencilState* NoDoubleBlendDSS;
+};
+
 #endif // RENDERSTATES_H
+
+/*
+//***************************************************************************************
+// RenderStates.h by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Defines render state objects.
+//***************************************************************************************
+
+#ifndef RENDERSTATES_H
+#define RENDERSTATES_H
+
+#include "d3dUtil.h"
+
+class RenderStates
+{
+public:
+static void InitAll(ID3D11Device* device);
+static void DestroyAll();
+
+static ID3D11RasterizerState* WireframeRS;
+static ID3D11RasterizerState* NoCullRS;
+
+static ID3D11BlendState* AlphaToCoverageBS;
+static ID3D11BlendState* TransparentBS;
+};
+
+#endif // RENDERSTATES_H
+*/
